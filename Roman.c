42c84a7ecb10182a516702d61bbb6d71108e97bc
AbhirupBasak026 <91425455+AbhirupBasak026@users.noreply.github.com>
2021-09-28 21:01:33 +0530

    Create Roman.c

---
+++ Roman.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void roman(int n)
+{
+    int
+ char A[9][3][3];
+    for()
+
+ for(int a=n;a!=0;a=a/10)
+}
+int main()
+{
+int a;
+printf("Enter the vslue of the number:")
+scanf("%d",&a)
+roman(a);
+return 0;
+}
