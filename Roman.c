77725686ee3e0e568d54e5ef940c158c5a37cb44
AbhirupBasak026 <91425455+AbhirupBasak026@users.noreply.github.com>
2021-09-29 10:54:24 +0530

    Add files via upload

--- Roman.c
+++ Roman.c
@@ -1,17 +1,63 @@
 #include<stdio.h>
-void roman(int n)
+#include<stdlib.h>
+int romans(char c)
 {
-    int
- char A[9][3][3];
-    for()
-
- for(int a=n;a!=0;a=a/10)
+    switch (c){
+        case'I':
+        return 1;
+        case 'V':
+        return 5;
+        case'X':
+        return 10;
+        case'L':
+        return 50;
+        case'C':
+        return 100;
+        case'D':
+        return 500;
+        case'M':
+        return 1000;
+        default:
+        return 0;
+    }
+}
+int tointegers(char *l){
+    int i,pn,s=0,n;
+    for(i=0;l[i]!='\0';i++){
+        pn=romans(l[i+1]);
+        n=romans(l[i]);
+        if(n<pn/10)
+            {
+             printf("Incorrect Numeral ");
+             return -1;
+            }
+        if(n<pn&&n>=pn/10)
+            {
+             s=s+pn-n;
+             i++;
+            }
+        else
+            s=s+n;
+    }
+    return s;
 }
 int main()
 {
-int a;
-printf("Enter the vslue of the number:")
-scanf("%d",&a)
-roman(a);
-return 0;
+    char k[10],ch;
+    int i,a;
+   // do{
+    printf("\nEnter the roman number you want to convert: \n");
+    for(i=0;i<=10;i++)
+    {
+        scanf("%c",&k[i]);
+        if(k[i]<65||k[i]>122)
+            break;
+    }
+    i=tointegers(k);
+    if(i>0)
+    printf("The number is %d",i);
+    printf("\nDo you want to continue?(y/n)");
+    scanf("%c",&ch);
+   // }while(ch=='y');
+    return 0;
 }
